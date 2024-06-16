a70df4d8b9f22017cdf1c09c386e5135608f51ac
eggmun98 <eggmun98@gmail.com>
2024-06-16 18:03:10 +0900

    chore: react-page-hook 패키지 초기 세팅

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import json from "@rollup/plugin-json";
+import babel from "@rollup/plugin-babel";
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const packageJson = require("./package.json");
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+      exports: "named",
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    json(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
+  ],
+  external: ["react", "react-dom", "styled-components"],
+};
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReactPageHook() {
+  return <div>React-Page-Hook</div>;
+}
